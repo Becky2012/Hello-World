5b1b0d0ce5de0a2b1274db7608c72829b274a907
Ubuntu <ubuntu@ip-172-31-81-102.ec2.internal>
2018-02-10 02:15:32 +0000

    add comment in code

--- geocode_impute.py
+++ geocode_impute.py
@@ -1,3 +1,4 @@
+#Read data in reference file as dictionary
 f=open("geo_ref.txt","r")
 new_dict={}
 i=0
@@ -5,10 +6,12 @@ for line in f:
     i=i+1
     if i>1:
         splitline=line.split()
+#Split keys in reference file into different length from 9 to 5 
         for i in range(9,4,-1):
             part_key=splitline[0][0:i]
             new_dict[part_key]=",".join(splitline[1:])
 print (new_dict.keys())
+#Identify missing values need to impute and save in a new file
 f1=open("geo_impute.txt","r")
 with open("geo_out.txt","w") as f1_out:
     i=0
@@ -19,6 +22,7 @@ with open("geo_out.txt","w") as f1_out:
        if i>1:
            eachline=line.strip("\n").split()
            if eachline[1]==".":
+#Match the ZIP with differenct length
                for j in range(9,4,-1):
                    if eachline[0][0:j] in new_dict.keys():
                       eachline[1]=new_dict[eachline[0][0:j]]
